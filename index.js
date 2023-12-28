20d9e2b653644f323538997bfcb87c7617378ef6
Avval Halani <aahalani@gmail.com>
2023-12-28 21:41:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+//Routes go here
+
+app.get("/events", async (req, res) => {
+  try {
+    const fetchedData = await mongoose.connection
+      .collection("events")
+      .find({})
+      .toArray();
+    res.json(fetchedData);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
+  }
+});
+
+//Connect to the database before listening
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
+  });
+});
